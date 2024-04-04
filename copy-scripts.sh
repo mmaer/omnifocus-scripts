1d2a320077610277e3cb6b8079747e086414307e
Kamil Kruczek <kamilkruczek0@hotmail.com>
2024-04-04 19:12:20 +0200

    Create estimateTime script

--- copy-scripts.sh
+++ copy-scripts.sh
@@ -1,4 +1,5 @@
 source .env
 
 cp scripts/linking/linking.omnijs "${OMNI_FOCUS_SCRIPT_FOLDER}/linking.omnijs"
-cp scripts/linking/linking.omnijs "${OMNI_FOCUS_SCRIPT_FOLDER}/updateDates.omnijs"
+cp scripts/updateDatesAndTime/updateDatesAndTime.omnijs "${OMNI_FOCUS_SCRIPT_FOLDER}/updateDatesAndTime.omnijs"
+cp scripts/estimateTime/estimateTime.omnijs "${OMNI_FOCUS_SCRIPT_FOLDER}/estimateTime.omnijs"
